0f81202bec9c9ec82cb043ccf18f90327ad40d14
Jitendra Dayma <jitendradayma@gmail.com>
2016-05-17 14:06:28 +0530

    code update on 17/05/2016
    code is changed to make flow of registration for exam proper.

--- module/candidate/exam_register_check.php
+++ module/candidate/exam_register_check.php
@@ -1,22 +1,34 @@
 <?php
-session_start();
-include_once('../../service/common/db_connection.php');
-$sql="SELECT exam_id FROM t_exam_org_qp WHERE exam_desc='{$_GET['q']}'";
-$result=mysqli_query($connection,$sql);
-$row = mysqli_fetch_assoc($result);
-$sql1="SELECT * FROM t_candidate_exam WHERE exam_id='$row['exam_id']'"; 
-$result1=mysqli_query($connection,$sql1);
-if($result){
+	//this page check whether user is already registered for exam or not and called register_exam
+	//modified by: Jitendra dayma
+	//modified on: 17/05/2016
+	
+	session_start();
+	include_once('../../service/common/db_connection.php');
+	include_once('../../lib/log4php/Logger.php');
+	Logger::configure('../../config/log_config.xml');
+	$log = Logger::getLogger('exam_register_check.php');
 
-header("register_exam.php");
-   exit;
+	$log->info("****START exam_register_check.php****");
 
-}
-else
-{
+	$sql="SELECT exam_id 
+		  FROM t_exam_org_qp 
+		  WHERE exam_name ='{$_GET['q']}'";
+	$result=mysqli_query($connection,$sql);
+	$row = mysqli_fetch_assoc($result);
+	$exam_id =$row['exam_id'];
 
- echo '<div class='error' style='display:none'>Exam already registered</div>';  
-
-}
-
+	
+	$sql1 ="SELECT *
+		   FROM t_candidate_exam 
+		   WHERE exam_id =$exam_id"; 
+	$result1=mysqli_query($connection,$sql1);
+	if(mysqli_num_rows($result1)===0){
+		header('Location: register_exam.php');
+	}
+	else
+	{
+		echo "<div class='error' style='display:none'>Exam already registered</div>";  
+	}
+	$log->info("****END exam_register_check.php****");
 ?>
